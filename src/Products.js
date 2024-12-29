80675b8390d893fbf893ffd2ff425ff5ec2545b8
LaurenCummings <LaurenCummings@users.noreply.github.com>
2024-12-29 17:42:07 -0500

    Initial commit

---
+++ src/Products.js
@@ -0,0 +1,133 @@
+import product1 from './images/1.png';
+import product2 from './images/2.png';
+import product3 from './images/3.png';
+import product4 from './images/4.png';
+import product5 from './images/5.png';
+import product6 from './images/6.png';
+import product7 from './images/7.png';
+import product8 from './images/8.png';
+import product9 from './images/9.jpg';
+import product10 from './images/10.png';
+import product11 from './images/11.png';
+import product12 from './images/12.png';
+import product13 from './images/13.png';
+import product14 from './images/14.png';
+import product15 from './images/15.png';
+import product16 from './images/16.png';
+
+const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Orange Purse",
+        price: 52.60,
+        tags: ["orange", "purse"],
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Blue T-Shirt",
+        price: 15.00,
+        tags: ["blue", "shirt", "t-shirt", "short-sleeve"],
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Black Hat",
+        price: 9.99,
+        tags: ["black", "hat", "fancy"],
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Pink Hat",
+        price: 12.99,
+        tags: ["pink", "hat"],
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Red Hat",
+        price: 11.99,
+        tags: ["red", "hat"],
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Blue Jacket",
+        price: 29.99,
+        tags: ["blue", "jacket", "fleece"],
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Brown Hat",
+        price: 9.99,
+        tags: ["brown", "hat"],
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Yellow and Blue Sweater",
+        price: 19.99,
+        tags: ["tie-dye", "sweatshirt", "blue", "yellow", "multicolor"],
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: "Brown Shoes",
+        price: 38.29,
+        tags: ["brown", "shoes", "fancy"],
+        productImage: product9,
+    },
+    {
+        id: 10,
+        productName: "Green Boots",
+        price: 29.99,
+        tags: ["green", "boots", "shoes"],
+        productImage: product10,
+    },
+    {
+        id: 11,
+        productName: "Brown Boots",
+        price: 29.99,
+        tags: ["brown", "boots", "tan", "shoes"],
+        productImage: product11,
+    },
+    {
+        id: 12,
+        productName: "Green Shorts",
+        price: 13.00,
+        tags: ["green", "short", "multicolor", "red", "blue"],
+        productImage: product12,
+    },
+    {
+        id: 13,
+        productName: "Pink Skirt",
+        price: 19.99,
+        tags: ["pink", "skirt"],
+        productImage: product13,
+    },
+    {
+        id: 14,
+        productName: "Green Shirt",
+        price: 15.00,
+        tags: ["green", "shirt", "t-shirt", "short-sleeve"],
+        productImage: product14,
+    },
+    {
+        id: 15,
+        productName: "Blue Shirt",
+        price: 15.00,
+        tags: ["blue", "shirt", "t-shirt", "short-sleeve"],
+        productImage: product15,
+    },
+    {
+        id: 16,
+        productName: "Black Shirt",
+        price: 15.00,
+        tags: ["black", "shirt", "short-sleeve", "t-shirt"],
+        productImage: product16,
+    },
+];
+
+export default PRODUCTS;
---
+++ src/components/Shop.js
@@ -0,0 +1,48 @@
+import './Shop.css';
+import PRODUCTS from '../Products';
+import Product from './Product';
+import { useState } from 'react';
+
+const Shop = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [displayedProducts, setDisplayedProducts] = useState(PRODUCTS);
+
+    const handleInputChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    const searchProducts = () => {
+        let selectedProducts = [];
+        for(let i = 1; i < PRODUCTS.length; i++) {
+            if (PRODUCTS[i].tags.includes(searchTerm)) {
+                selectedProducts.push(PRODUCTS[i]);
+            }
+        }
+        setDisplayedProducts(selectedProducts);
+    };
+
+    return (
+        <div className="shop">
+            <div className="shopTitle">
+                <h1>The Shop</h1>
+            </div>
+            <div>
+                <input 
+                    placeholder="Search for products" 
+                    type="text" 
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                />
+                <button onClick={searchProducts}>Search</button>
+            </div>
+            <div className="products">
+                {displayedProducts.map((product) => (
+                    <Product data={product} />
+                ))}
+            </div>
+        </div>
+    )
+};
+
+export default Shop;
