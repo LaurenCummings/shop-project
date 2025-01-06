417eefd8cc24869d400785db6de8ff10e6f59e87
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-01-06 00:00:58 -0500

    Changed title color and text

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -25,7 +25,7 @@ const Shop = () => {
     return (
         <div className="shop">
             <div className="shopTitle">
-                <h1>The Shop</h1>
+                <h1>Annie's Handmade Pottery</h1>
             </div>
             <div>
                 <input 
