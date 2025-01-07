f60cef57caca01e88535a7b458c9d637dabfa37e
LaurenCummings <LaurenCummings@users.noreply.github.com>
2025-01-06 23:12:14 -0500

    Added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import { ShoppingCart } from 'phosphor-react';
+import logo from '../images/logo.png';
 
 const Navbar = () => {
     return (
         <div className="navbar">
+            <img src={logo} alt="logo" />
             <div className="links">
                 <Link to="/"> Shop </Link>
                 <Link to="/cart">
