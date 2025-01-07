6951cb8e49c7a7c533c13eac4a01b907a1b8d23e
LaurenCummings <LaurenCummings@users.noreply.github.com>
2025-01-06 23:10:51 -0500

    Added .toFixed(2) to price

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,17 +2,17 @@ import './CartItem.css';
 import { useCart } from '../contexts/ShopContext';
 
 const CartItem = (props) => {
-    const { id, productName, price, productImage } = props.data;
+    const { id, productName, price, productImages } = props.data;
     const { cartItems, addToCart, removeFromCart, updateCartItemCount } = useCart();
 
     return (
         <div className="cartItem">
-            <img src={productImage} />
+            <img src={productImages[0]} alt={productName} />
             <div className="description">
                 <p>
                     <b>{productName}</b>
                 </p>
-                <p>${price}</p>
+                <p>${price.toFixed(2)}</p>
                 <div className="countHandler">
                     <button onClick={() => removeFromCart(id)}> - </button>
                     <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)} />
