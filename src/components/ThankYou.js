5b8714be232369a379abd970b790401b2919d0f3
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-01-17 00:27:41 -0500

    Deleted comments

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,23 +1,11 @@
 import './ThankYou.css';
 import { useCart } from '../contexts/ShopContext';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const ThankYou = () => {
     const { getTotalCartAmount, clearCart } = useCart();
     const [totalAmount, setTotalAmount] = useState(getTotalCartAmount());
     {clearCart()}
-
-    // useEffect(() => {
-    //     const handleBeforeUnload = (event) => {
-    //         setTotalAmount(0);
-    //     };
-
-    //     window.addEventListener('beforeunload', handleBeforeUnload);
-
-    //     return () => {
-    //         window.removeEventListener('beforeunload', handleBeforeUnload);
-    //     };
-    // }, []);
 
     return (
         <div className="thankYou">
