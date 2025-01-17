166d8826ec750deff0e794733e7825b6f40200c4
LaurenCummings <LaurenCummings@users.noreply.github.com>
2025-01-17 00:14:31 -0500

    Got ThankYou page to show correct total
    Made changes so ThankYou page shows correct total before clearing cart. Might still be an issue if user refreshes page.

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,20 +1,28 @@
 import './ThankYou.css';
 import { useCart } from '../contexts/ShopContext';
-// import { useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const ThankYou = () => {
     const { getTotalCartAmount, clearCart } = useCart();
-    // const totalAmount = useRef();
+    const [totalAmount, setTotalAmount] = useState(getTotalCartAmount());
+    {clearCart()}
 
     // useEffect(() => {
-    //     totalAmount.current = getTotalCartAmount();
-    // });
+    //     const handleBeforeUnload = (event) => {
+    //         setTotalAmount(0);
+    //     };
+
+    //     window.addEventListener('beforeunload', handleBeforeUnload);
+
+    //     return () => {
+    //         window.removeEventListener('beforeunload', handleBeforeUnload);
+    //     };
+    // }, []);
 
     return (
         <div className="thankYou">
             <p>Thank you for trying out the demo.</p>
-                {/* You spent ${ totalAmount.current }. */}
-                {clearCart()}
+            <p>You spent ${ totalAmount }.</p> 
             <p>Your cart has been cleared.</p>
         </div>
     )
