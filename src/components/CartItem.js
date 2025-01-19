8694055cfc75ec427363c6682d56d9555c85b67d
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-01-19 15:11:53 -0500

    Added X to top right of each cart item to delete item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,6 @@
 import './CartItem.css';
 import { useCart } from '../contexts/ShopContext';
+import { XCircle } from 'phosphor-react';
 
 const CartItem = (props) => {
     const { id, productName, price, productImages } = props.data;
@@ -7,18 +8,21 @@ const CartItem = (props) => {
 
     return (
         <div className="cartItem">
-            <img src={productImages[0]} alt={productName} />
-            <div className="description">
-                <p>
-                    <b>{productName}</b>
-                </p>
-                <p>${price.toFixed(2)}</p>
-                <div className="countHandler">
-                    <button onClick={() => removeFromCart(id)}> - </button>
-                    <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)} />
-                    <button onClick={() => addToCart(id)}> + </button>
+            <button className="deleteItemX">
+                <XCircle size={32} />
+            </button>
+                <img src={productImages[0]} alt={productName} />
+                <div className="description">
+                    <p>
+                        <b>{productName}</b>
+                    </p>
+                    <p>${price.toFixed(2)}</p>
+                    <div className="countHandler">
+                        <button onClick={() => removeFromCart(id)}> - </button>
+                        <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)} />
+                        <button onClick={() => addToCart(id)}> + </button>
+                    </div>
                 </div>
-            </div>
         </div>
     );
 };
