9983807bd0496bf5192e8ada5300b260309a3f7e
LaurenCummings <LaurenCummings@users.noreply.github.com>
2025-01-20 00:00:20 -0500

    Added hamburger menu
    Hamburger menu and side menu for links when browser/screen gets smaller

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-import { ShoppingCart } from 'phosphor-react';
+import { ShoppingCart, List } from 'phosphor-react';
 import logo from '../images/logo.png';
 import { useCart } from '../contexts/ShopContext';
 
 const Navbar = () => {
     const { getNumCartItems } = useCart();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen((open) => !open);
+        console.log("Toggled");
+    }
 
     return (
         <div className="navbar">
             <img src={logo} alt="logo" />
-            <div className="links">
+            <div className={`links ${isOpen ? "menuOpen" : ""}`}>
                 <Link to="/"> Shop </Link>
                 <Link to="/cart">
                     <ShoppingCart className="cartIcon" size={32} /> 
                     <p className="itemCount">({getNumCartItems()})</p>
                 </Link>
+            </div>
+            <div>
+                <button className="hamburgerMenu" onClick={toggleMenu}>
+                   <List size={42} /> 
+                </button>
             </div>
         </div>
     )
