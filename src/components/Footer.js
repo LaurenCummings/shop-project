a793d79ae3a313ccf1189b1c2a8c119d9b7b6bd2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-01-26 20:50:42 -0500

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import './Footer.css';
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            <p>
+                Disclaimer: This website was created as a 
+                practice project. The shop and its owner
+                are works of fiction.
+            </p>
+        </div>
+    )
+}
+
+export default Footer;
