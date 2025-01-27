fb03be464f88ea7917c494206467e6ef4376267c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-01-26 21:14:30 -0500

    Added logos to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import './Footer.css';
+import { FacebookLogo, TwitterLogo, InstagramLogo} from 'phosphor-react';
 
 const Footer = () => {
 
     return (
         <div className="footer">
+            <div className="logos">
+                <FacebookLogo size={32} />
+                <TwitterLogo size={32} />
+                <InstagramLogo size={32} />
+            </div>
             <p>
                 Disclaimer: This website was created as a 
                 practice project. The shop and its owner
