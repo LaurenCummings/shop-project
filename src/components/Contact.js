646a82b541b7f523407a25383509562418ecbbf9
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-16 03:48:53 -0400

    Styled the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,13 +7,13 @@ const Contact = () => {
             <form>
                 <label for="name">Name: </label>
                 <input type="text" id="name" name="name" placeholder="Name" />
-                <br />
+
                 <label for="email">Email: </label>
                 <input type="text" id="email" name="email" placeholder="Email" />
-                <br />
+
                 <label for="message">Message: </label>
                 <textarea id="message" name="message" placeholder="Message" />
-                <br />
+
                 <input type="submit" value="Submit" />
             </form>
         </div>
