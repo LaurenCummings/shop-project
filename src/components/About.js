e7dbee47a6ccff3a4b42edb4f01c33335c731d63
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-16 01:02:14 -0400

    Changed the about page image and layout

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,29 @@
 import './About.css';
-import aboutImg from '../images/about-img.webp';
+import aboutImg from '../images/about-img.jpg';
 
 const About = () => {
 
     return (
         <div className="about">
             <h1>About the Shop</h1>
-            <img src={aboutImg} alt="pottery image" />
-            <p>
-                Annie grew up on a farm in Montana. 
-                As a child she would spend hours playing
-                in the mud, making temporary creations
-                that would soon be washed away. 
-                She was gifted her first pottery wheel at age 15
-                and the rest is history. 
-            </p>
-            <br />
-            <p>
-                After graduating with a degree in Fine Arts
-                from the University of Montana, Annie 
-                opened her studio in Faketown, Montana,
-                where she can be seen regularly creating
-                new pieces.
-            </p>
+            <div className="about-content">
+                <img src={aboutImg} alt="pottery image" />
+                <p>
+                    Annie grew up on a farm in Montana. 
+                    As a child she would spend hours playing
+                    in the mud, making temporary creations
+                    that would soon be washed away. 
+                    She was gifted her first pottery wheel at age 15
+                    and the rest is history. 
+                    <br />
+                    <br />
+                    After graduating with a degree in Fine Arts
+                    from the University of Montana, Annie 
+                    opened her studio in Faketown, Montana,
+                    where she can be seen regularly creating
+                    new pieces.
+                </p>
+            </div>
         </div>
     )
 };
