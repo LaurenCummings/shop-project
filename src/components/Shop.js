a23f3fed23b9fa5dcc41d606692e241f62152ddc
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-16 22:35:00 -0400

    Deleted search feature

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,50 +1,16 @@
 import './Shop.css';
 import PRODUCTS from '../Products';
 import Product from './Product';
-import { useState } from 'react';
-import { MagnifyingGlass } from 'phosphor-react';
 
 const Shop = () => {
-
-    const [searchTerm, setSearchTerm] = useState('');
-    const [displayedProducts, setDisplayedProducts] = useState(PRODUCTS);
-
-    const handleInputChange = (event) => {
-        setSearchTerm(event.target.value);
-    }
-
-    const searchProducts = () => {
-        let selectedProducts = [];
-        if (searchTerm == "") {
-            selectedProducts = PRODUCTS;
-        } else {
-            for(let i = 1; i < PRODUCTS.length; i++) {
-                if (PRODUCTS[i].tags.includes(searchTerm)) {
-                    selectedProducts.push(PRODUCTS[i]);
-                }
-            }
-            setDisplayedProducts(selectedProducts); 
-        }
-    };
 
     return (
         <div className="shop">
             <div className="shopTitle">
                 <h1>Our Products</h1>
             </div>
-            <div className="searchBar">
-                <input 
-                    placeholder="Search for products" 
-                    type="text" 
-                    value={searchTerm}
-                    onChange={handleInputChange}
-                />
-                <button onClick={searchProducts}>
-                    <MagnifyingGlass size={22}></MagnifyingGlass>
-                </button>
-            </div>
             <div className="products">
-                {displayedProducts.map((product) => (
+                {PRODUCTS.map((product) => (
                     <Product data={product} />
                 ))}
             </div>
