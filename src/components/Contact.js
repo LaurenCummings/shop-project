67738ed52524436044c3e70bd520e47bcd5e71f9
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-03-18 03:38:10 -0400

    Capturing contact form input with state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,50 @@
 import './Contact.css';
+import { useState } from 'react';
 
 const Contact = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(
+            name,
+            email,
+            message
+        );
+    };
 
     return (
         <div className="contact">
             <form>
-                <label for="name">Name: </label>
-                <input type="text" id="name" name="name" placeholder="Name" required/>
+                <label>Name: </label>
+                <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    placeholder="Name" 
+                    onChange={(e) => setName(e.target.value)}
+                    required/>
 
-                <label for="email">Email: </label>
-                <input type="text" id="email" name="email" placeholder="Email" required/>
+                <label>Email: </label>
+                <input 
+                    type="text" 
+                    id="email" 
+                    name="email" 
+                    placeholder="Email" 
+                    onChange={(e) => setEmail(e.target.value)}
+                    required/>
 
-                <label for="message">Message: </label>
-                <textarea id="message" name="message" placeholder="Message" required/>
+                <label>Message: </label>
+                <textarea 
+                    id="message" 
+                    name="message" 
+                    placeholder="Message" 
+                    onChange={(e) => setMessage(e.target.value)}
+                    required/>
 
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" onClick={(e) => handleSubmit(e)} />
             </form>
         </div>
     )
